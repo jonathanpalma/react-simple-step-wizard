630180b6a1077e9f912e01c7ff31b5a1450c3d82
Jonathan Palma <tanpalma04@gmail.com>
2019-09-29 21:52:18 -0600

    Created basic wizard with static navigator. Updated react dependency from 16.2 to 16.3 in order to support getDerivedStateFromProps.

--- src/Wizard.tsx
+++ src/Wizard.tsx
@@ -1,7 +1,63 @@
 import * as React from 'react';
 
-function Wizard(): JSX.Element {
-  return <div>This is a Wizard component</div>;
+interface Props {
+  children: JSX.Element[] | JSX.Element;
+}
+
+interface State {
+  currentStep: number;
+  totalSteps: number;
+}
+
+class Wizard extends React.Component<Props, State> {
+  state = { currentStep: 0, totalSteps: -1 };
+
+  static getDerivedStateFromProps(props: Props, state: State) {
+    const totalSteps = React.Children.count(props.children);
+    return totalSteps !== state.totalSteps ? { totalSteps } : null;
+  }
+
+  goToNextStep = (): void => {
+    this.setState(prevState => ({ currentStep: prevState.currentStep + 1 }));
+  };
+
+  goToPrevStep = (): void => {
+    this.setState(prevState => ({ currentStep: prevState.currentStep - 1 }));
+  };
+
+  render() {
+    const { children } = this.props;
+    const { currentStep, totalSteps } = this.state;
+    return (
+      <div>
+        {totalSteps > 0
+          ? React.Children.map(
+              children,
+              (childElement: React.ReactElement, index) =>
+                currentStep === index
+                  ? React.cloneElement(childElement, {})
+                  : null
+            )
+          : null}
+        <div className="wizard-navigator">
+          <button
+            type="button"
+            onClick={this.goToPrevStep}
+            disabled={currentStep === 0}
+          >
+            Previous
+          </button>
+          <button
+            type="button"
+            onClick={this.goToNextStep}
+            disabled={currentStep === totalSteps - 1}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Wizard;
