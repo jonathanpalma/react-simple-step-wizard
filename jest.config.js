f6371f76a76ed05e48481138710f25ee61ac3f81
Jonathan Palma <tanpalma04@gmail.com>
2019-10-13 11:56:50 -0600

    chore(jest): config jest

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  transform: {
+    '\\.(ts|tsx)?$': 'ts-jest',
+  },
+  testMatch: ['<rootDir>/src/**/?(*.)test.{ts,tsx}'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  // preset: 'ts-jest',
+  // testEnvironment: 'node',
+  testPathIgnorePatterns: ['/node_modules/', '/public/'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+};
