1b22329b860a96e0cd8dbcf1f0d6e48f0966d6cb
Jonathan Palma <tanpalma04@gmail.com>
2019-10-23 00:40:25 -0600

    test(navigator): fix navigator tests

--- src/components/__tests__/Navigator.test.tsx
+++ src/components/__tests__/Navigator.test.tsx
@@ -3,11 +3,46 @@ import { render } from '@testing-library/react';
 import Navigator from '../Navigator';
 import Wizard from '../Wizard';
 
-it('renders Navigator component', () => {
-  const { debug } = render(
-    <Wizard>
-      <Navigator />
-    </Wizard>
-  );
-  debug();
+describe('Navigator', () => {
+  it('renders custom Navigator', () => {
+    const { container } = render(
+      <Wizard>
+        <Navigator>
+          {({
+            getFirstStepProps,
+            getLastStepProps,
+            getNextStepProps,
+            getPrevStepProps,
+          }) => (
+            <div>
+              <button type="button" {...getFirstStepProps()}>
+                &lt;&lt; First
+              </button>
+              <button type="button" {...getPrevStepProps()}>
+                &lt; Back
+              </button>
+              <button type="button" {...getNextStepProps()}>
+                Next &gt;
+              </button>
+              <button type="button" {...getLastStepProps()}>
+                Last &gt;&gt;
+              </button>
+            </div>
+          )}
+        </Navigator>
+      </Wizard>
+    );
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('renders default Navigator', () => {
+    const { container } = render(
+      <Wizard>
+        <Navigator />
+      </Wizard>
+    );
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
 });
